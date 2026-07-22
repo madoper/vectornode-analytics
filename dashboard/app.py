4ccf262b6c490da8ad54b2e269eda6f0ebed4612
madoper <madoper@mail.ru>
2026-07-22 14:39:46 +0300

    Hello World with timestamp

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import time
 
 st.set_page_config(page_title="TEST", layout="wide")
 
 st.title("Hello World")
+st.write("Time:", time.time())
 st.write("If you can read this, Streamlit works")
+
+st.sidebar.write("Sidebar test:", time.time())
